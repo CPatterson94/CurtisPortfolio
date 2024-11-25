33ca0b41c6cc7de4be7c2328e46622ad462db2d4
Curtis Patterson <Dpatter94@yahoo.com>
2024-11-25 17:55:40 -0500

    Added some Portfolio files, cleaned up the directory, and added a .gitignore file.

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Nav from "./components/Nav";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+    </>
+  );
+}
---
+++ Portfolio/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <h2>Home</h2>
+          <h2>About</h2>
+          <h2>Projects</h2>
+          <h2>Resume</h2>
+          <h2>Contact</h2>
+        </ul>
+      </nav>
+    </>
+  );
+}
