8081c7a1d34f7303d33742ab6613fa0567c3bddb
Curtis Patterson <Dpatter94@yahoo.com>
2024-12-02 16:30:20 -0500

    Added basic styling too Home

--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="home-container">
         <h1>Home</h1>
         <p>
           Welcome to my portfolio! My name is Daniel Patterson, and I am a web
           developer. I am passionate about creating responsive and accessible
           web applications that help make the web a better place for everyone. I
-          am proficient in HTML, CSS, and JavaScript, and I am always looking to
-          learn new technologies and frameworks. Please take a look at my
-          projects and resume, and feel free to contact me if you have any
+          am proficient in JavaScript, CSS, and HTML, and I am always looking to
+          learn new languages, technologies and frameworks. Please take a look
+          at my projects and resume, and feel free to contact me if you have any
           questions or would like to work together.
         </p>
       </div>
