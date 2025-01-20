4a4227c2e2bc337c5c83a29f6b5bf7303de2050a
Curtis Patterson <Dpatter94@yahoo.com>
2025-01-20 11:57:18 -0500

    Small edits, listed below:
    Updated home page, added Under Construction message to all pages

--- Portfolio/src/pages/contactMe.jsx
+++ Portfolio/src/pages/contactMe.jsx
@@ -3,15 +3,7 @@ export default function Contact() {
     <>
       <div className="contact-container">
         <h1>Contact Me</h1>
-        <p>
-          Welcome to my portfolio! My name is Daniel Patterson, and I am a web
-          developer. I am passionate about creating responsive and accessible
-          web applications that help make the web a better place for everyone. I
-          am proficient in JavaScript, CSS, and HTML, and I am always looking to
-          learn new languages, technologies and frameworks. Please take a look
-          at my projects and resume, and feel free to contact me if you have any
-          questions or would like to work together.
-        </p>
+        <p>Under Construction</p>
       </div>
     </>
   );
--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -4,13 +4,20 @@ export default function Home() {
       <div className="home-container">
         <h1>Home</h1>
         <p>
-          Welcome to my portfolio! My name is Daniel Patterson, and I am a web
-          developer. I am passionate about creating responsive and accessible
-          web applications that help make the web a better place for everyone. I
-          am proficient in JavaScript, CSS, and HTML, and I am always looking to
-          learn new languages, technologies and frameworks. Please take a look
-          at my projects and resume, and feel free to contact me if you have any
-          questions or would like to work together.
+          I’m Daniel Patterson, a web developer based in Alpharetta, GA, serving
+          metro Atlanta and surrounding areas. While I’m new to the tech
+          industry, I’ve spent the last 15 years working at a hockey rink and
+          coaching, which taught me a lot about teamwork, problem-solving, and
+          constantly learning new things—skills I bring into every project I
+          work on. I have a focus on front-end development with JavaScript,
+          HTML5, and CSS, and I’ve been diving into React, Vue, and Angular. On
+          the back-end, I work with Node, Express, PostgreSQL, and Prisma. I’m
+          also familiar with Git for version control and use SaSS for styling.
+          I’m always excited to learn more and take on new challenges, so if
+          there’s a tech I don’t know yet, I’m all in to pick it up! When I’m
+          not coding, you’ll find me on the ice playing hockey, DJing, gaming,
+          or getting lost in a good book. I’m excited to collaborate with you
+          and help bring your web development projects to life.
         </p>
       </div>
     </>
